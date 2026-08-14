594821e2d73b21a07e5931a8022fee7d70036e2a
MADHU <adepumadhu2655@gmail.com>
2026-08-14 10:10:53 +0530

    frontend

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,28 @@
-alert("HELLO FROM JAVA SCRIPT");
+const users = [
+    {
+        "name" : "Jhon Doe",
+        "gender" : "Male",
+        "image" : "john.png"
+    },
+    {
+        "name" : "Jane Doe",
+        "gender" : "Female",
+        "image" : "john.png"
+    }
+]
+
+var idx = 0;
+
+const toggleuser = ()=>{
+    if(idx == 0){
+        idx = 1;
+    }else{
+        idx = 0;
+    }
+    document.getElementById("im").src = users[idx].image;
+    document.getElementById("name").innerText = users[idx].name;
+    document.getElementById("gender").innerText = users[idx].gender;
+}
+
+document.getElementById("toggle").addEventListener("click",toggleuser);
+
